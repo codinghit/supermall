38294a070a179c883bd58f33de47dac5499749a5
LiZhuangHao <1444879567@qq.com>
2020-02-14 15:53:20 +0800

    首页开发

---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <scroll class="content">
+      <ul>
+        <li>个人信息1</li>
+        <li>个人信息2</li>
+        <li>个人信息3</li>
+        <li>个人信息4</li>
+        <li>个人信息5</li>
+        <li>个人信息6</li>
+        <li>个人信息7</li>
+        <li>个人信息8</li>
+        <li>个人信息9</li>
+        <li>个人信息10</li>
+      </ul>
+    </scroll>
+  </div>
+</template>
+
+<script>
+  import Scroll from 'components/common/scroll/Scroll'
+
+  export default {
+    name: "Profile",
+    components: {
+      Scroll
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
